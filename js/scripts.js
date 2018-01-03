fd3b02ec4999dfc1684f542ba7524767a1187fd2
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-01-03 10:30:58 +0100

    edit scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-function getTriangleArea - function getTriangleArea(a, h) {};
+function getTriangleArea(a, h) {
+	if ((a >= 0) && (h >= 0)) {
+		return a * h / 2;
+	}
+	else {
+		return 'Niepoprawne dane';
+	}
+}
 
-if ( (a >= 0) && (h >= 0) ) {
-	return TriangleArea = a * h / 2;
-}
-else {
-	console.log('Niepoprawne dane');
-}
-console.log('getTriangleArea(10, 6)');
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(5, 11);
-var triangle3Area = getTriangleArea(7, 14);
+var triangle3Area = getTriangleArea(-7, 14);
+console.log(triangle1Area, triangle2Area, triangle3Area);
